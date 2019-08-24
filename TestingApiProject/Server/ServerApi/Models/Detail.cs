38b1a3647125b015cea909903125cb4e7f624132
Giemecs <carlguo26@gmail.com>
2019-08-25 00:22:51 +0800

    add mongodb

--- TestingApiProject/Server/ServerApi/Models/Detail.cs
+++ TestingApiProject/Server/ServerApi/Models/Detail.cs
@@ -1,10 +1,19 @@
 using System;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
 
 namespace ServerApi.Models
 {
     public class Detail
     {
-        public DateTime RecordTime { get; set; }
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public int RecordId { get; set; }
+
+        public int Id { get; set; }
+
+        [BsonElement("RecordTime")]
+        public DateTime Time { get; set; }
         public decimal Amount { get; set; }
         public string Description { get; set; }
     }
